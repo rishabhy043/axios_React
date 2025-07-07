dc07b4d53b83eee7b5a4d97ea9ae9660436b8270
rishabhy043 <rishabh.knipss043@gmail.com>
2025-07-07 20:03:37 +0530

    commit Second

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
 // import { PiCloudFogBold } from "react-icons/pi";npm install react-icons@latest
 
+/*
 export const Movie = () => {
 
     const API = "https://www.omdbapi.com/?i=tt3896198&apikey=1c12799f&s=titanic&page=1";
@@ -25,5 +27,41 @@ export const Movie = () => {
 
     return (<>
         <h1>Movie</h1>
+    </>)
+}
+*/
+
+//------------2nd  STEP TO STORE THE DATA AFTER RETRIVEING---------------
+
+
+export const Movie = () => {
+   const [Data, setData] = useState([]);
+
+    const API = "https://www.omdbapi.com/?i=tt3896198&apikey=1c12799f&s=titanic&page=1";
+    //    const res = await axios.get(API);
+    //    console.log(res);
+
+    const getMovieData = async () => {
+        try {
+            const res = await axios.get(API);
+            // console.log(res.data.search);
+            console.log(res);
+             setData(res.data.search)     //storing the data
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getMovieData();
+    }, [])
+
+    return (<>
+        <ul>
+            {Data.map((curElem) =>{
+            return <Card key = {curElem.imdbID}  movieData={curElem}/>
+            })
+            }
+        </ul>
     </>)
 }
