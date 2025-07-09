fa75af5fe0c600d41baf8d5427000a4137816649
rishabhy043 <rishabh.knipss043@gmail.com>
2025-07-09 22:19:13 +0530

    Fetched Data by - AXIOS

---
+++ src/API/PostAPI.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const api = axios.create({ // creates instanse of axios
+    baseURL:"https://jsonplaceholder.typicode.com"  // baseURL --> which always sames f4all http requests 
+});
+
+//get Method
+
+export const GetPost =(() =>{
+    return api.get("/posts")
+})
+// console.log(GetPost);
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import './App.css'
-import { Movie } from './pages/Movie'
+import { GetPost } from "./API/PostAPI";
+import { useEffect } from "react";
 
-function App() {
-  return (<>
-<Movie></Movie>
-  </>)
+const App = () =>{
+  console.log(GetPost());
+
+  const getPostData = async () =>{
+    const res = await GetPost();
+    console.log(res);
+  };
+
+  useEffect(() =>{
+    getPostData();
+  } ,[])
+
+  return(<>
+  <h1>Hello kse ho ?</h1></>)
 }
 
-export default App
+export default App;
