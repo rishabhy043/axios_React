d177efae7bd0c4cd6883dd075f1f306b3e9d1ea1
rishabhy043 <rishabh.knipss043@gmail.com>
2025-12-01 13:24:48 +0530

    Step 1 completed

--- src/Rdx/ReduxApp.jsx
+++ src/Rdx/ReduxApp.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
 
-const ReduxApp = () => {
-  return (
-    <div>Hello Rishabh are you learing the Redux</div>
-  )
+const ADD_TASK = "task/add";
+const DELETE_TASK = "task/delete";
+
+const initialState = {
+  task: [],
 }
+
+const ReduxApp = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TASK:
+      return {
+        ...state,
+        task: [...state.task, action.payload],
+      };
+
+    case DELETE_TASK:
+      return {
+        ...state,
+        task: [...state.task, action.payload],
+      }
+
+    default:
+      return state;
+  }
+};
 
 export default ReduxApp;
